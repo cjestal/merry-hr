4b9be67359cbf4f8808fc30f811666cd8fb46fa7
Clint James <cjestal@gmail.com>
2024-05-06 15:25:00 -0400

    slide over

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,13 +1,17 @@
 <template>
-	<div class="flex flex-col gap-3">
-		<BreadCrumbs />
-		<h1 class="text-2xl font-bold">{{ title }}</h1>
-		<p class="text-sm text-gray-500 mb-2">{{ description }}</p>
+	<div class="md:flex md:items-center md:justify-between">
+		<div class="flex flex-col gap-3">
+			<BreadCrumbs />
+			<h1 class="text-2xl font-bold">{{ title }}</h1>
+			<p class="text-sm text-gray-500 mb-2">{{ description }}</p>
+		</div>
+		<div class="flex gap-3">
+			<slot />
+		</div>
 	</div>
 </template>
 
 <script setup>
-
 defineProps({
 	title: String,
 	description: String,
--- pages/Employees.vue
+++ pages/Employees.vue
@@ -1,6 +1,15 @@
 <template>
 	<SitePage>
-		<PageHeader title="Employee Management" description="Add, update, or remove employee details" />
+		<PageHeader
+			title="Employee Management"
+			description="Add, update, or remove employee details"
+		>
+			<SiteSlideOver
+				label="New Employee"
+				title="New Employee"
+				description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+			/>
+		</PageHeader>
 		<SimpleTable title="Employees" />
 	</SitePage>
 </template>
